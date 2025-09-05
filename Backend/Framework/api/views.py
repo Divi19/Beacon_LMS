9dcbf0575657927f3ebc6969324adda36e9d09ed
Divieleisch Ranjan <DivieleischRanjan19052005@gmail.com>
2025-09-06 01:14:23 +0800

    Add course_creation view to handle form submission - Implemented course_creation view in views.py to handle GET and POST requests - Processes form submissions with proper validation and saves valid course data - Renders course.html sample template for user interaction

--- Backend/Framework/api/views.py
+++ Backend/Framework/api/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render, redirect
 from .forms import CoursesForm
 
 # Create your views here.
+def course_creation(request):
+    # form = CoursesForm()
+    if request.method == 'POST':
+        form = CoursesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('create_courses')
+    else:
+        form = CoursesForm()
+
+    return render(request, 'course.html', {'form' : form})
