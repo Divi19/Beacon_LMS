227855d8f776cb38ed81efdca38d2283b86065f0
mork00011 <mork0001@student.monash.edu>
2025-09-05 22:15:31 +0800

    Added functions for entrypage along with css module file.

---
+++ Frontend/beacon-react-app/src/pages/EntryPage/EntryPage.jsx
@@ -0,0 +1,45 @@
+// src/pages/EntryPage.jsx
+import { useState } from "react";
+import Button from "../../components/Button/Button";
+import s from "./EntryPage.module.css";
+import { useNavigate } from "react-router-dom";
+
+
+export default function EntryPage() {
+  const [role, setRole] = useState(null);
+  const navigate = useNavigate();
+
+  const go = (r) => {
+    const next =
+      r === "student" ? "/student/my-courses" : "/instructor/courses/new";
+    navigate(next);
+  };
+
+  return (
+    <div className={s.shell}>
+      {/* LEFT PANEL */}
+      <section className={s.left}>
+        <div className={s.logoWrap}>
+          <img src="/logo.svg" alt="Beacon Logo" className={s.logoImg} />
+          <div className={s.slogan}>“Brillare Luminoso”</div>
+        </div>
+      </section>
+
+      {/* RIGHT PANEL */}
+      <section className={s.right}>
+        <div className={s.card}>
+          <h1 className={s.title}>Choose</h1>
+          <div className={s.buttons}>
+            <Button variant="teal" onClick={() => go("student")}>
+              Student
+            </Button>
+            <div className={s.or}>OR</div>
+            <Button variant="orange" onClick={() => go("instructor")}>
+              Instructor
+            </Button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
