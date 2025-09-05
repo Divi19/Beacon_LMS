51b36d1eea2b256472acf6401ae1f6bcbfca92d4
Divieleisch Ranjan <dran0025@student.monash.edu>
2025-09-06 00:53:07 +0800

    Add Course model to models.py file - Created Course model with fields such as course id and title - Set course_id to unique to prevent duplicate course entries - Selected each field type appropriately to match requirements

--- Backend/Framework/api/models.py
+++ Backend/Framework/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    course_title = models.CharField(max_length=250)
+    course_id = models.CharField(max_length=25, unique=True)
+    course_credits = models.IntegerField()
+    course_director = models.CharField(max_length=50)
+    course_description = models.TextField(max_length=1000)
+    
