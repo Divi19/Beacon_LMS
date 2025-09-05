856b27481a0a75c6e0827ed360e746e574c50913
mork00011 <mork0001@student.monash.edu>
2025-09-05 22:45:36 +0800

    Updated button component and installed relevant packages to run the app

---
+++ Frontend/beacon-react-app/src/pages/StudentEnrollment/StudentEnrollment.jsx
@@ -0,0 +1,8 @@
+export default function StudentEnrollment(){
+  return (
+    <div style={{padding: 24}}>
+      <h1>Enrollment</h1>
+      <p>We'll build this page next. For now, this is a placeholder route.</p>
+    </div>
+  );
+}
