5973f8093a7994e0e9fe93e62003196aec71921f
Divieleisch Ranjan <DivieleischRanjan19052005@gmail.com>
2025-09-06 08:28:11 +0800

    Update REST permissions and add frontend route - Modified the REST_FRAMEWORK settings in settings.py to allow unrestricted access - Retained JWTAuthentication for future authentication use - Added a path in main urls.py to expose FrontendView at the root URL for API access

--- Backend/Framework/backend/settings.py
+++ Backend/Framework/backend/settings.py
@@ -39,6 +39,9 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_PERMISSION_CLASSES":[
+        "rest_framework.permissions.AllowAny"
+    ],
 }
 
 SIMPLE_JWT = {
