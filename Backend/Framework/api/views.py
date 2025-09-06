a6d6c58a9b329fac3f0d9426037c8361785c1342
Divieleisch Ranjan <DivieleischRanjan19052005@gmail.com>
2025-09-06 08:22:59 +0800

    Add FrontendView APIView for Course Model - Implemented FrontendView using Django REST Framework APIView - Provides GET endpoint to retrieve all Course objects as JSON - Provides POST endpoints to cerate new Course instances through the CourseSerializer

--- Backend/Framework/api/views.py
+++ Backend/Framework/api/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from .forms import CoursesForm
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import CourseSerializer
+from .models import Course
 
 # Create your views here.
 def course_creation(request):
-    # form = CoursesForm()
     if request.method == 'POST':
         form = CoursesForm(request.POST)
         if form.is_valid():
@@ -13,3 +16,20 @@ def course_creation(request):
         form = CoursesForm()
 
     return render(request, 'course.html', {'form' : form})
+
+class FrontendView(APIView):
+    def get(self, request):
+        courses = Course.objects.all()
+        output = [{"course_title": course.course_title,
+                   "course_id": course.course_id,
+                   "course_credits": course.course_credits,
+                   "course_director": course.course_director,
+                   "course_description": course.course_description}
+                   for course in courses]
+        return Response(output)
+    
+    def post(self, request):
+        serializer = CourseSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
