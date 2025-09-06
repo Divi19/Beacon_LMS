d63c3cd5cb12b50a7f24a29431352da0680f285c
Divieleisch Ranjan <DivieleischRanjan19052005@gmail.com>
2025-09-06 08:14:34 +0800

    Update model to have str method and add Serializer - Added __str__ method to the Course model to return information in a more readable format - Created the serializers.py in the app file with CourseSerializer using all Course fields - Prepares Course model for API serialization and improves debugging

--- Backend/Framework/api/models.py
+++ Backend/Framework/api/models.py
@@ -7,4 +7,6 @@ class Course(models.Model):
     course_credits = models.IntegerField()
     course_director = models.CharField(max_length=50)
     course_description = models.TextField(max_length=1000)
-    
+
+    def __str__(self):
+        return f"{self.course_id}: {self.course_title}"
