f4a67fbe774162fd56ae7a5a15b3b318c7c829a0
ISABELLA <isab0008@student.monash.edu>
2025-09-18 21:33:06 +0800

    "Finish setting up device changes"

--- Backend/Framework/backend/settings.py
+++ Backend/Framework/backend/settings.py
@@ -102,11 +102,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_lms_db',
+        'USER': 'myuser',
+        'PASSWORD': 'Bella021',  # replace with your password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
