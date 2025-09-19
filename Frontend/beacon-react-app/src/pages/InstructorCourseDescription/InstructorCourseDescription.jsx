d8e4e0a8df4e30d4cc10cb8bb54c9f5f8aa32379
ISABELLA <isab0008@student.monash.edu>
2025-09-19 11:00:55 +0800

    US2 - Finish Implementing the new update on the instructor course description page

--- Frontend/beacon-react-app/src/pages/InstructorCourseDescription/InstructorCourseDescription.jsx
+++ Frontend/beacon-react-app/src/pages/InstructorCourseDescription/InstructorCourseDescription.jsx
@@ -10,6 +10,7 @@ export default function InstructorCourseDescription() {
   const { courseId } = useParams();
   const navigate = useNavigate();
   const { enroll, isEnrolled } = useEnrollment();
+  const [showLessons, setShowLessons] = useState(false);
 
   // const course = useMemo(
   //   () => courses.find((c) => c.id === courseId),
@@ -67,8 +68,118 @@ export default function InstructorCourseDescription() {
               <span className={s.noLessons}>No lessons</span>
             )}
           </div>
+          <br />
+          <br />
+          <Button variant="orange"
+                      className={s.enrollBtn}
+                      onClick={() => navigate("/instructor/course-list")}
+                    >
+                      <span>Back to Courses</span>
+                      <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="22"
+                    height="22"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="white"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <circle cx="12" cy="12" r="10" />
+                    <polyline points="12 8 16 12 12 16" />
+                    <line x1="8" y1="12" x2="16" y2="12" />
+                  </svg>
+                    </Button>
           </div>
         </div>
+    <div className={s.wrap}>
+  <div className={s.row1}>
+    <div className={s.panel1}>
+      <h2 className={s.label}>Enrolled Students</h2>
+    </div>
+
+    <div className={s.panel1}
+      onClick={() => setShowLessons(!showLessons)} 
+     style={{ cursor: "pointer" }}>
+  <h2 className={s.label}>Lessons</h2>
+    </div>
+
+    <div className={s.panel1}>
+      <h2 className={s.label}>Active Classrooms</h2>
+    </div>
+  </div>
+</div>
+{showLessons && (
+  <div className={s.lessonsCard}>
+    <h2 className={s.lessonsLabel}>Lessons</h2>
+
+    <div className={s.container}>
+      {course.lessons && course.lessons.length > 0 ? (
+        course.lessons.map((lesson, idx) => (
+          <div key={idx} className={s.card} style={{ cursor: "pointer" }}>
+            <h2 className={s.cardTitle}>{lesson.title}</h2>
+
+            <div className={s.cardDesc1}>
+        <div className={s.leftGroup}>
+          <span>Code:</span>
+          <span className={s.spacing}><strong>{lesson.code}</strong></span>
+        </div>
+      </div>
+
+      <div className={s.cardDesc2}>
+        <span>Course Director:</span>
+        <span>{lesson.director}</span>
+      </div>
+
+      <div className={s.cardDesc3}>
+        <span>Duration:</span>
+        <span>{lesson.duration}</span>
+      </div>
+    </div>
+        ))
+      ) : (
+        <div className={s.card} style={{ cursor: "pointer" }}>
+          <h2 className={s.cardTitle}>Lesson 1</h2>
+          <div className={s.cardDesc1}>
+            <div className={s.leftGroup}>
+              <span>Code:</span>
+            </div>
+          </div>
+          <div className={s.cardDesc2}>
+            <span>Course Director:</span>
+          </div>
+          <div className={s.cardDesc3}>
+            <span>Duration:</span>
+          </div>
+          <br />
+          <br />
+          <Button variant="orange"
+                      className={s.enrollBtn}
+                      onClick={() => navigate("/instructor/lesson-creation")}
+                    >
+                      <span>Create</span>
+                      <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="22"
+                    height="22"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="white"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <circle cx="12" cy="12" r="10" />
+                    <polyline points="12 8 16 12 12 16" />
+                    <line x1="8" y1="12" x2="16" y2="12" />
+                  </svg>
+                    </Button>
+        </div>
+      )}
+    </div>
+  </div>
+)}
     </>
   );
 }
