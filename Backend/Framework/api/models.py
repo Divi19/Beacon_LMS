64392eff1e5ac35937fe39eba0af73465ebff713
Divieleisch Ranjan <DivieleischRanjan19052005@gmail.com>
2025-09-23 19:49:59 +0800

    (US2)Refactor Django models and standardize schema - Standardize primary keys and rename user tables to users - Update Course and Lesson models: column names, field mappings and defaults - Comment out deprecated fields while preserving db_table and managed=True, and also removed the save() methods

--- Backend/Framework/api/models.py
+++ Backend/Framework/api/models.py
@@ -29,7 +29,7 @@ class User(models.Model):
 
     class Meta:
         managed = True
-        db_table = 'user'
+        db_table = 'users'
 
     @property 
     def is_authenticated(self):
@@ -51,33 +51,33 @@ class InstructorProfile(models.Model):
 #        db_table = "course"
 #        managed = False
 
-class Course(models.Model):
-    course_title = models.CharField(max_length=250)
-    course_id = models.CharField(primary_key=True, max_length=500, unique=True)
-    course_credits = models.IntegerField()
-    course_director = models.CharField(max_length=50)
-    course_description = models.TextField(max_length=1000)
-    course_number_of_lessons = models.IntegerField(default=0)
+# class Course(models.Model):
+#     course_title = models.CharField(max_length=250)
+#     course_id = models.CharField(primary_key=True, max_length=500, unique=True)
+#     course_credits = models.IntegerField()
+#     course_director = models.CharField(max_length=50)
+#     course_description = models.TextField(max_length=1000)
+#     course_number_of_lessons = models.IntegerField(default=0)
 
-    def __str__(self):
-        return f"{self.course_id}: {self.course_title}"
+#     def __str__(self):
+#         return f"{self.course_id}: {self.course_title}"
     
-class Lesson(models.Model):
-    lesson_title = models.CharField(blank=True, null=True, max_length=250)
-    lesson_id = models.CharField(primary_key=True, max_length = 100, unique=True, editable=True)
-    lesson_credits = models.IntegerField(blank=True, null=True,)
-    lesson_duration = models.IntegerField(blank=True, null=True, default=0)
-    lesson_description = models.TextField(blank=True, null=True, max_length=1000)
-    lesson_objective = models.TextField(blank=True, null=True, max_length=1000)
-    lesson_prerequisite = models.TextField(blank=True, null=True, max_length=500)
-    courses = models.ForeignKey(Course, related_name="lessons", on_delete=models.CASCADE)
-    # slot_index = models.IntegerField(blank=True, null=True)
+# class Lesson(models.Model):
+#     lesson_title = models.CharField(blank=True, null=True, max_length=250)
+#     lesson_id = models.CharField(primary_key=True, max_length = 100, unique=True, editable=True)
+#     lesson_credits = models.IntegerField(blank=True, null=True,)
+#     lesson_duration = models.IntegerField(blank=True, null=True, default=0)
+#     lesson_description = models.TextField(blank=True, null=True, max_length=1000)
+#     lesson_objective = models.TextField(blank=True, null=True, max_length=1000)
+#     lesson_prerequisite = models.TextField(blank=True, null=True, max_length=500)
+#     courses = models.ForeignKey(Course, related_name="lessons", on_delete=models.CASCADE)
+#     # slot_index = models.IntegerField(blank=True, null=True)
 
-    # class Meta:
-    #     unique_together = ("courses", "slot_index")
+#     # class Meta:
+#     #     unique_together = ("courses", "slot_index")
     
-    def __str__(self):
-        return f"{self.lesson_id} : {self.lesson_title}"
+#     def __str__(self):
+#         return f"{self.lesson_id} : {self.lesson_title}"
 
     # def save(self, *args, **kwargs):
     #     if self.slot_index is None:  # only assign if not manually provided
@@ -107,25 +107,28 @@ class Course(models.Model):
         INACTIVE = "Inactive", "INACTIVE"
         DRAFT = "Draft", "DRAFT"
 
-    course_id = models.CharField(primary_key=True, max_length=6, unique=True, default=generate_custom_id, editable=False)
-    title = models.CharField(max_length=255,null=True, blank=True)
-    status = models.CharField(max_length=50, choices=CourseStatus.choices, default=CourseStatus.ACTIVE)
-    owner_instructor = models.ForeignKey('InstructorProfile', models.DO_NOTHING,  null=True, blank=True,)
-    credits = models.PositiveIntegerField(blank=False, null=True, default=30,  validators=[MinValueValidator(1), MaxValueValidator(30)],)
-    description = models.TextField(blank=True, null=True)
+    course_id = models.AutoField(primary_key=True)    
+    code = models.CharField(max_length=20, unique=True, null=True, blank=True, db_column="code")
+    course_title = models.CharField(max_length=255,null=True, blank=True, db_column="title")
+    course_credits = models.PositiveIntegerField(blank=False, null=True, default=30,  validators=[MinValueValidator(1), MaxValueValidator(30)], db_column="credits")
+    course_director = models.CharField(max_length=50, null=True, blank=True, db_column="director")
+    course_description = models.TextField(blank=True, null=True, db_column="description")
+    course_status = models.CharField(max_length=50, choices=CourseStatus.choices, default=CourseStatus.ACTIVE, db_column="status")
+    course_number_of_lessons = models.IntegerField(default=0, db_column="number_of_lessons")
+    owner_instructor = models.ForeignKey('InstructorProfile', models.DO_NOTHING,  null=True, blank=True, db_column="owner_instructor_id")
 
     class Meta:
         managed = True
         db_table = 'course'
 
-    def save(self, *args, **kwargs):
-        if not self.course_id:
-            # ensure uniqueness: regenerate if collision occurs
-            new_id = generate_custom_id()
-            while Course.objects.filter(course_id=new_id).exists():
-                new_id = generate_custom_id()
-            self.course_id = new_id
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     if not self.course_id:
+    #         # ensure uniqueness: regenerate if collision occurs
+    #         new_id = generate_custom_id()
+    #         while Course.objects.filter(course_id=new_id).exists():
+    #             new_id = generate_custom_id()
+    #         self.course_id = new_id
+    #     super().save(*args, **kwargs)
 
 
 class CourseDraft(models.Model):
@@ -175,29 +178,30 @@ class Lesson(models.Model):
         TWO = 2, "2"
         THREE = 3, "3"
         FOUR = 4 , "4"
-    lesson_id =  models.CharField(primary_key=True, max_length=6, unique=True, default=generate_custom_id, editable=False)
-    course = models.ForeignKey(Course, models.DO_NOTHING, null=True, blank=True,)
-    title = models.CharField(max_length=255, null=True, blank=True)
-    description = models.TextField(blank=True, null=True)
-    objectives = models.TextField(blank=True, null=True)
-    duration_weeks = models.PositiveIntegerField(blank=True, null=True, choices=DurationWeeks.choices, default=DurationWeeks.FOUR)
-    status = models.CharField(max_length=50, choices=LessonStatus.choices, default=LessonStatus.ACTIVE)
-    #is_active = models.BooleanField(null=True, blank=True, default=True)
-    created_by = models.ForeignKey(InstructorProfile, models.DO_NOTHING,null=True, blank=True, db_column='created_by')
-    created_at = models.DateTimeField(auto_now_add=True)
+    lesson_id = models.AutoField(primary_key=True, db_column="lesson_id")
+    # lesson_code = models.CharField(max_length=6, unique=True, default=generate_custom_id)
+    courses = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True, db_column="course_id", related_name="lessons")
+    lesson_title = models.CharField(max_length=255, null=True, blank=True, db_column="title")
+    lesson_description = models.TextField(blank=True, null=True, db_column="description")
+    lesson_objectives = models.TextField(blank=True, null=True, db_column="objectives")
+    lesson_duration_weeks = models.PositiveIntegerField(blank=True, null=True, choices=DurationWeeks.choices, default=DurationWeeks.FOUR, db_column="duration_weeks")
+    lesson_status = models.CharField(max_length=50, choices=LessonStatus.choices, default=LessonStatus.ACTIVE, db_column="status")
+    lesson_created_by = models.ForeignKey(InstructorProfile, models.DO_NOTHING,null=True, blank=True, db_column='created_by')
+    lesson_created_at = models.DateTimeField(default=timezone.now, db_column="created_at")
+    lesson_prerequisite = models.TextField(blank=True, null=True, max_length=500, db_column="prerequisite")
 
     class Meta:
         managed = True
         db_table = 'lesson'
 
-    def save(self, *args, **kwargs):
-        if not self.lesson_id:
-            # ensure uniqueness: regenerate if collision occurs
-            new_id = generate_custom_id()
-            while Lesson.objects.filter(lesson_id=new_id).exists():
-                new_id = generate_custom_id()
-            self.lesson_id = new_id
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     if not self.lesson_id:
+    #         # ensure uniqueness: regenerate if collision occurs
+    #         new_id = generate_custom_id()
+    #         while Lesson.objects.filter(lesson_id=new_id).exists():
+    #             new_id = generate_custom_id()
+    #         self.lesson_id = new_id
+    #     super().save(*args, **kwargs)
 
 
 class LessonEnrollment(models.Model):
@@ -212,16 +216,16 @@ class LessonEnrollment(models.Model):
         db_table = 'lesson_enrollment'
 
 
-class LessonPrerequisite(models.Model):
-    id = models.BigAutoField(primary_key=True) 
-    #lesson_id = models.ForeignKey(Lesson, models.DO_NOTHING)
-    #pk = models.CompositePrimaryKey('lesson_id', 'prereq_lesson_id')
-    lesson = models.ForeignKey(Lesson, models.DO_NOTHING, null=True, blank=True,)
-    prereq_lesson = models.ForeignKey(Lesson, models.DO_NOTHING, null=True, blank=True, related_name='lessonprerequisite_prereq_lesson_set')
+# class LessonPrerequisite(models.Model):
+#     id = models.BigAutoField(primary_key=True) 
+#     #lesson_id = models.ForeignKey(Lesson, models.DO_NOTHING)
+#     #pk = models.CompositePrimaryKey('lesson_id', 'prereq_lesson_id')
+#     lesson = models.ForeignKey(Lesson, models.DO_NOTHING, null=True, blank=True,)
+#     prereq_lesson = models.ForeignKey(Lesson, models.DO_NOTHING, null=True, blank=True, related_name='lessonprerequisite_prereq_lesson_set')
 
-    class Meta:
-        managed = True
-        db_table = 'lesson_prerequisite'
+#     class Meta:
+#         managed = True
+#         db_table = 'lesson_prerequisite'
 
 """
 Classroom
