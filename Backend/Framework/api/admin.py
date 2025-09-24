6c13350e249aea73a0ceef10141d00a1ebabcb8b
mandy-wong-yan <mwon0077@student.monash.edu>
2025-09-24 23:23:38 +0800

    Successful integration into master - Resolved remaining conflicts

--- Backend/Framework/api/admin.py
+++ Backend/Framework/api/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from .models import *
 
-<<<<<<< HEAD
 # Register your models here.
 
 admin.site.register(Course)
-=======
-
-admin.site.register(Course)
-admin.site.register(Lesson)
->>>>>>> origin/Frontend_Isabella
 admin.site.register(CourseDraft)
 admin.site.register(StudentProfile)
 admin.site.register(InstructorProfile)
@@ -17,11 +11,7 @@ admin.site.register(User)
 admin.site.register(Enrollment)
 admin.site.register(Classroom)
 admin.site.register(ClassroomEnrollment)
-<<<<<<< HEAD
 admin.site.register(Lesson)
-=======
-
->>>>>>> origin/Frontend_Isabella
 admin.site.register(LessonEnrollment)
 admin.site.register(LessonPrerequisite)
 
