f950f37bcf13c591dcb58896e8570e4079dc7dc1
ISABELLA <isab0008@student.monash.edu>
2025-09-24 20:58:46 +0800

    Remaining edited file because of the merged

--- Backend/Framework/api/admin.py
+++ Backend/Framework/api/admin.py
@@ -1,21 +1,9 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-# class LessonInLine(admin.TabularInline):
-#     model = Lesson
-#     extra = 1
-
-# class CourseAdmin(admin.ModelAdmin):
-#     # inlines = [LessonInLine]
-
 
 admin.site.register(Course)
 admin.site.register(Lesson)
-# admin.site.register(Student)
-# Register your models here.
-
-# admin.site.register(Course)
 admin.site.register(CourseDraft)
 admin.site.register(StudentProfile)
 admin.site.register(InstructorProfile)
@@ -25,7 +13,6 @@ admin.site.register(Enrollment)
 admin.site.register(Classroom)
 admin.site.register(ClassroomEnrollment)
 
-# admin.site.register(Lesson)
-# admin.site.register(LessonEnrollment)
-# admin.site.register(LessonPrerequisite)
+admin.site.register(LessonEnrollment)
+admin.site.register(LessonPrerequisite)
 
