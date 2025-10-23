1606edb411cd072297a3fef9af4e9b2d85210e8e
Divi19 <DivieleischRanjan19052005@gmail.com>
2025-10-23 10:27:24 +0800

    Update InstructorLessonCreate.jsx

--- Frontend/beacon-react-app/src/pages/Instructor/Instructor Lesson Create/InstructorLessonCreate.jsx
+++ Frontend/beacon-react-app/src/pages/Instructor/Instructor Lesson Create/InstructorLessonCreate.jsx
@@ -41,7 +41,7 @@ export default function InstructorLessonCreation({ onCourseCreated }) {
     time_start: "",
     time_end: "",
     zoom_link: "",
-    supervisor: "",
+    supervisor_input: "",
     duration_weeks: "",
   });
   const [onlineClassrooms, setOnlineClassrooms] = useState([]);
